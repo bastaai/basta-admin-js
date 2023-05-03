21fa04bc56cab41252c2ebac309ffea3d73e25df
Viktor Blöndal <viktor.blondal@basta.ai>
2023-05-03 09:26:38 +0000

    .

---
+++ examples/dist.ts
@@ -0,0 +1,5 @@
+import { BastaAdmin, Sale } from '../dist';
+
+const bastaAdmin: BastaAdmin = new BastaAdmin('sk');
+
+const sale: Sale = await bastaAdmin.createSale();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './basta-admin';
+export * from '../types';
