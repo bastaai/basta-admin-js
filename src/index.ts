575e0b35f7985f7d0962880fa9a791dfcfe6faf1
Viktor Blöndal <viktor.blondal@basta.ai>
2023-05-05 12:42:20 +0000

    Export configs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
-import { BastaAdmin } from './basta-admin';
+import { BastaRequest } from '../types/request';
+import {
+  IBastaAdmin,
+  ISaleService,
+  IBidService,
+  IUserService,
+} from '../types/sdk';
+import { BidService } from './services/bid-service';
+import { SaleService } from './services/sale-service';
+import { UserService } from './services/user-service';
+
+class BastaAdmin implements IBastaAdmin {
+  readonly sale: ISaleService;
+  readonly bid: IBidService;
+  readonly user: IUserService;
+
+  protected readonly _bastaReq: BastaRequest;
+
+  constructor(secretKey: string, accountId: string) {
+    this._bastaReq = {
+      accountId: accountId,
+      url: 'https://management.api.stage.basta.ai/graphql',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': secretKey,
+      },
+    };
+
+    this.sale = new SaleService(this._bastaReq);
+    this.bid = new BidService(this._bastaReq);
+    this.user = new UserService(this._bastaReq);
+  }
+}
 
 export { Sale } from '../types/sale';
 export default BastaAdmin;
