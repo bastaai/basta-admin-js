40907f6f17853a7ae1bc7d3c1900b05e29ad99a3
Viktor Blöndal <viktor.blondal@basta.ai>
2023-05-06 00:03:21 +0000

    Gearing into alpha mode.

--- examples/dist.ts
+++ examples/dist.ts
@@ -1,6 +1,7 @@
-import BastaAdmin, { Sale } from '../dist/src';
+import BastaAdmin from '../dist';
+import { Sale } from '../types';
 
 const bastaAdmin: BastaAdmin = new BastaAdmin('sk', 'accid');
 
-const sale: Sale = await bastaAdmin.sale.create();
-bastaAdmin.user.refreshUserToken();
+const sale: Sale = await bastaAdmin.sale.get();
+sale.accountId;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,4 @@
 export * from './sale';
 export * from './user';
+
+export const BastaAdmin: (secretKey: string, accountId: string) => void;
